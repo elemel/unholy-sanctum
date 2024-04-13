680e12834dcafa6f45a1b998c36e0a0833518a03
Mikael Lind <elemel@elemel.se>
2024-04-13 21:26:44 +0200

    Add camera script

---
+++ scripts/camera.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+class_name Camera
+
+func _physics_process(_delta: float) -> void:
+    var target = find_target()
+
+    if target != null:
+        position = target.position
+
+func find_target() -> Node2D:
+    for unit in get_parent().get_children():
+        if unit is Necromancer:
+            return unit
+
+    return null

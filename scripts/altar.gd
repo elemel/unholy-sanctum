9f90c47a660369722306c2dae40b3db3e062bcd0
Mikael Lind <elemel@elemel.se>
2024-04-13 20:31:42 +0200

    Receive damage

--- scripts/altar.gd
+++ scripts/altar.gd
@@ -1,5 +1,13 @@
 extends StaticBody2D
 class_name Altar
 
+@export var health = 200.0
+
 func _physics_process(_delta):
     z_index = int(position.y)
+
+func receive_damage(damage: float):
+    health -= damage
+
+    if health < 0.0:
+        queue_free()
